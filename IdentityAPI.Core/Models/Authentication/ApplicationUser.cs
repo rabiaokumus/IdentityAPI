d8f0b729925da11311df203d560fb36ea81014c5
rabiaokumus <rabiaokumus@gmail.com>
2020-12-28 00:19:50 +0300

    Add project files.

---
+++ IdentityAPI.Core/Models/Authentication/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdentityAPI.Core.Models.Authentication
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
---
+++ IdentityAPI.Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using IdentityAPI.Core.Models.Authentication;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdentityAPI.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
