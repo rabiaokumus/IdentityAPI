2919a93504bcb816ca8eacb51a2bad0c221603f0
rabiaokumus <rabiaokumus@gmail.com>
2020-12-28 00:50:25 +0300

    Edit

---
+++ IdentityAPI.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityAPI.Core.UnitOfWorks
+{
+    public interface IUnitOfWork
+    {
+        Task CommitAsync();
+
+        void Commit();
+    }
+}
--- IdentityAPI/Controllers/AuthenticateController.cs
+++ IdentityAPI/Controllers/AuthenticateController.cs
@@ -17,14 +17,14 @@ namespace IdentityAPI.Controllers
 {
     public class AuthenticateController : Controller
     {
-        private readonly UserManager<ApplicationUser> userManager;
-        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
         private readonly IConfiguration _configuration;
 
         public AuthenticateController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
         {
-            this.userManager = userManager;
-            this.roleManager = roleManager;
+            _userManager = userManager;
+            _roleManager = roleManager;
             _configuration = configuration;
         }
 
@@ -32,10 +32,11 @@ namespace IdentityAPI.Controllers
         [Route("login")]
         public async Task<IActionResult> Login([FromBody] LoginModel model)
         {
-            var user = await userManager.FindByNameAsync(model.Username);
-            if (user != null && await userManager.CheckPasswordAsync(user, model.Password))
+            var user = await _userManager.FindByNameAsync(model.Username);
+
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
             {
-                var userRoles = await userManager.GetRolesAsync(user);
+                var userRoles = await _userManager.GetRolesAsync(user);
 
                 var authClaims = new List<Claim>
                 {
@@ -64,6 +65,7 @@ namespace IdentityAPI.Controllers
                     expiration = token.ValidTo
                 });
             }
+            
             return Unauthorized();
         }
 
@@ -71,7 +73,7 @@ namespace IdentityAPI.Controllers
         [Route("register")]
         public async Task<IActionResult> Register([FromBody] RegisterModel model)
         {
-            var userExists = await userManager.FindByNameAsync(model.Username);
+            var userExists = await _userManager.FindByNameAsync(model.Username);
             if (userExists != null)
                 return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User already exists!" });
 
@@ -81,7 +83,7 @@ namespace IdentityAPI.Controllers
                 SecurityStamp = Guid.NewGuid().ToString(),
                 UserName = model.Username
             };
-            var result = await userManager.CreateAsync(user, model.Password);
+            var result = await _userManager.CreateAsync(user, model.Password);
             if (!result.Succeeded)
                 return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User creation failed! Please check user details and try again." });
 
@@ -92,7 +94,7 @@ namespace IdentityAPI.Controllers
         [Route("register-admin")]
         public async Task<IActionResult> RegisterAdmin([FromBody] RegisterModel model)
         {
-            var userExists = await userManager.FindByNameAsync(model.Username);
+            var userExists = await _userManager.FindByNameAsync(model.Username);
             if (userExists != null)
                 return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User already exists!" });
 
@@ -102,18 +104,18 @@ namespace IdentityAPI.Controllers
                 SecurityStamp = Guid.NewGuid().ToString(),
                 UserName = model.Username
             };
-            var result = await userManager.CreateAsync(user, model.Password);
+            var result = await _userManager.CreateAsync(user, model.Password);
             if (!result.Succeeded)
                 return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User creation failed! Please check user details and try again." });
 
-            if (!await roleManager.RoleExistsAsync(IdentityAPI.Core.Roles.UserRoles.Admin))
-                await roleManager.CreateAsync(new IdentityRole(IdentityAPI.Core.Roles.UserRoles.Admin));
-            if (!await roleManager.RoleExistsAsync(IdentityAPI.Core.Roles.UserRoles.User))
-                await roleManager.CreateAsync(new IdentityRole(IdentityAPI.Core.Roles.UserRoles.User));
+            if (!await _roleManager.RoleExistsAsync(IdentityAPI.Core.Roles.UserRoles.Admin))
+                await _roleManager.CreateAsync(new IdentityRole(IdentityAPI.Core.Roles.UserRoles.Admin));
+            if (!await _roleManager.RoleExistsAsync(IdentityAPI.Core.Roles.UserRoles.User))
+                await _roleManager.CreateAsync(new IdentityRole(IdentityAPI.Core.Roles.UserRoles.User));
 
-            if (await roleManager.RoleExistsAsync(IdentityAPI.Core.Roles.UserRoles.Admin))
+            if (await _roleManager.RoleExistsAsync(IdentityAPI.Core.Roles.UserRoles.Admin))
             {
-                await userManager.AddToRoleAsync(user, IdentityAPI.Core.Roles.UserRoles.Admin);
+                await _userManager.AddToRoleAsync(user, IdentityAPI.Core.Roles.UserRoles.Admin);
             }
 
             return Ok(new Response { Status = "Success", Message = "User created successfully!" });
